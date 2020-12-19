01d5d5b4725c64ec90b7e4f2e0e9f6d8b9aa3a27
Ronny Ancorini <ronnyancorini@outlook.com>
2020-12-19 22:19:56 +0100

    Refactor: extract method

--- CamelCaseMethod/CamelCaseMethod/StringExtensions.cs
+++ CamelCaseMethod/CamelCaseMethod/StringExtensions.cs
@@ -3,10 +3,15 @@ using System.Linq;
 
 namespace CamelCaseMethod {
     public static class StringExtensions {
+        private const string Separator = " ";
+
         public static string CamelCase(this string str) {
+            static string ToUpperCamelCase(string word) => 
+                $"{word[0].ToString().ToUpper()}{word.Substring(1)}";
+
             return str
-                .Split(" ")
-                .Select(word => $"{word[0].ToString().ToUpper()}{word.Substring(1)}")
+                .Split(Separator)
+                .Select(ToUpperCamelCase)
                 .Aggregate((acc, word) => acc + word);
         }
     }
