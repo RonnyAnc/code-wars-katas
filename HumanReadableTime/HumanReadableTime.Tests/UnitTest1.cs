5ea393b2e537aa95348f553742df874974904793
Ronny Ancorini <ronnyancorini@outlook.com>
2021-01-08 18:09:28 +0100

    Add HumanReadableTime kata

---
+++ HumanReadableTime/HumanReadableTime.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace HumanReadableTime.Tests {
+    public class TimeFormatShould {
+        [Test]
+        public void Test1() {
+            Assert.AreEqual("00:00:00", TimeFormat.GetReadableTime(0));
+            Assert.AreEqual("00:00:05", TimeFormat.GetReadableTime(5));
+            Assert.AreEqual("00:01:00", TimeFormat.GetReadableTime(60));
+            Assert.AreEqual("23:59:59", TimeFormat.GetReadableTime(86399));
+            Assert.AreEqual("99:59:59", TimeFormat.GetReadableTime(359999));
+        }
+    }
+}
---
+++ HumanReadableTime/HumanReadableTime/TimeFormat.cs
@@ -0,0 +1,7 @@
+namespace HumanReadableTime {
+    public class TimeFormat {
+        public static string GetReadableTime(int seconds) {
+            return "00:00:00";
+        }
+    }
+}
