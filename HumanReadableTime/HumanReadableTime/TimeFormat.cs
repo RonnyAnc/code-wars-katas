f18fd4f09937acdf756f49878ab054613064a704
Ronny Ancorini <ronnyancorini@outlook.com>
2021-01-08 18:40:54 +0100

    Green all

--- HumanReadableTime/HumanReadableTime/TimeFormat.cs
+++ HumanReadableTime/HumanReadableTime/TimeFormat.cs
@@ -1,7 +1,11 @@
 namespace HumanReadableTime {
     public class TimeFormat {
         public static string GetReadableTime(int allSeconds) {
-            return $"00:00:{allSeconds:D2}";
+            var hours = allSeconds / 3600;
+            var remainingSeconds = allSeconds % 3600;
+            var minutes = remainingSeconds / 60;
+            var seconds = remainingSeconds % 60;
+            return $"{hours:D2}:{minutes:D2}:{seconds:D2}";
         }
     }
 }
